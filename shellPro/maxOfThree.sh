f21f71ec3f803930b03a06a088b0afa08b863904
Nitin Chawla <chawlanr@gmail.com>
2023-06-25 16:34:34 +0530

    welcome to User to our Program

---
+++ shellPro/maxOfThree.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This script reads First and Last name of user and welcomes them
+
+read -p "Enter your First Name: " firstName
+
+read -p "Enter your Last Name: " lastName
+
+echo "Hi, $firstName $lastName! Welcome to Shell Learning Program"
+
