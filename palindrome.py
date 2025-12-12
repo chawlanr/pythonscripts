547a540ee1ecb9b2868f2a96d88080b16413e8f8
Nitin Chawla <chawlanr@gmail.com>
2025-12-12 18:40:33 +0530

    Added new file palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,22 @@
+string=input("Enter any string: ")
+# if(string not in " \n\t"):
+    # string=(string.replace(" ","")).upper()
+    # if(string == string[::-1]):
+        # print("Entered string is Palindrome")
+    # else:
+        # print("Entered string is not Palindrome")
+# else:
+    # print("Entered is either empty string or not a string")
+count=0
+if(string not in " \n\t"):
+    half=len(string)//2
+    for x in range(half):
+        if(string[x]==string[-(x+1)]):
+            count+=1
+            continue
+    if(count==half):
+        print("Entered string is Palindrome")
+    else:
+        print("Entered string is not Palindrome")
+else:
+    print("Entered is either empty string or not a string")
